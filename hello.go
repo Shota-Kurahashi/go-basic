b39f3b01b79d8abf0dc3b3c62bf6a6e64e4f6dea
Shota-Kurahashi <Myheroes0715@gmail.com>
2023-06-12 23:07:51 +0900

    hello world

---
+++ hello.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"hello/greeting"
+
+	"rsc.io/quote"
+)
+func main() {
+    fmt.Println("Hello, World!")
+		fmt.Println(quote.Go())
+		world()
+		greeting.Hello("Gladys")
+	
+}
